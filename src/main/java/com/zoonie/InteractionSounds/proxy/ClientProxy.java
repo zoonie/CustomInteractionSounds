9212d560626a9e043e8f143a8018b305745ef35c
zoonie <zooonie@outlook.com>
2015-02-14 16:13:32 +0000

    Load mappings server side

--- src/main/java/com/zoonie/InteractionSounds/proxy/ClientProxy.java
+++ src/main/java/com/zoonie/InteractionSounds/proxy/ClientProxy.java
@@ -1,15 +1,11 @@
 package com.zoonie.InteractionSounds.proxy;
 
-import java.io.File;
-
 import javax.swing.UIManager;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-import com.zoonie.InteractionSounds.InteractionSounds;
-import com.zoonie.InteractionSounds.configuration.MappingsConfigManager;
 import com.zoonie.InteractionSounds.interaction.InteractionHandler;
 import com.zoonie.InteractionSounds.interaction.KeyBindings;
 import com.zoonie.InteractionSounds.interaction.KeyInputHandler;
@@ -26,7 +22,7 @@ public class ClientProxy extends CommonProxy
 		handlerSetup();
 		UISetup();
 		soundSetup();
-		configSetup();
+		super.mappingsConfigSetup();
 	}
 
 	private void handlerSetup()
@@ -56,10 +52,5 @@ public class ClientProxy extends CommonProxy
 		MinecraftForge.EVENT_BUS.register(new SoundEventHandler());
 
 		SoundHandler.getSounds();
-	}
-
-	private void configSetup()
-	{
-		new MappingsConfigManager(new File("sounds" + File.separator + InteractionSounds.MOD_NAME + File.separator + InteractionSounds.MODID + ".json"));
 	}
 }
--- src/main/java/com/zoonie/InteractionSounds/proxy/CommonProxy.java
+++ src/main/java/com/zoonie/InteractionSounds/proxy/CommonProxy.java
@@ -6,7 +6,9 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import com.zoonie.InteractionSounds.InteractionSounds;
 import com.zoonie.InteractionSounds.configuration.Config;
+import com.zoonie.InteractionSounds.configuration.MappingsConfigManager;
 import com.zoonie.InteractionSounds.network.ServerHandler;
 
 public class CommonProxy
@@ -15,6 +17,7 @@ public class CommonProxy
 	{
 		handlerSetup();
 		configSetup(event.getSuggestedConfigurationFile());
+		mappingsConfigSetup();
 	}
 
 	private void handlerSetup()
@@ -27,5 +30,10 @@ public class CommonProxy
 		Configuration config = new Configuration(file);
 		new Config(config);
 		config.save();
+	}
+
+	public void mappingsConfigSetup()
+	{
+		new MappingsConfigManager(new File("sounds" + File.separator + InteractionSounds.MOD_NAME + File.separator + InteractionSounds.MODID + ".json"));
 	}
 }

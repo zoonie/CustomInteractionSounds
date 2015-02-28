b83719435fe7367d754312fe049caeb555376667
zoonie <zooonie@outlook.com>
2015-02-28 03:07:33 +0000

    Update build version

--- src/main/java/com/zoonie/interactionsounds/InteractionSounds.java
+++ src/main/java/com/zoonie/interactionsounds/InteractionSounds.java
@@ -20,7 +20,7 @@ public class InteractionSounds
 {
 	public static final String MOD_NAME = "Interaction Sounds";
 	public static final String MODID = "InteractionSounds";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.3";
 
 	public static final Logger logger = LogManager.getLogger(MOD_NAME);
 
